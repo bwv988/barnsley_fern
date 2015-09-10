ed69c82d2d9fa4b4a314b952b8f6b1729ceeabbe
bwv988 <ralph.schlosser@gmail.com>
2015-09-10 19:05:37 +0000

    Improved code.

--- assets/js/fern.js
+++ assets/js/fern.js
@@ -3,89 +3,93 @@
 
 "use strict";
 
-// Define the affine transformation parameters.
-var A1 = math.matrix([[0.85, 0.04], [-0.04, 0.85]]);
-var A2 = math.matrix([[0.2, -0.26], [0.23, 0.22]]);
-var A3 = math.matrix([[-0.15, 0.28], [0.26, 0.24]]);
-var A4 = math.matrix([[0, 0], [0, 0.16]]);
+var SCREENX = 800;
+var SCREENY = 800;
+var SCALE = 70;
+var MAX_ITER = 5000;
 
-var b1 = math.matrix([0, 1.6]);
-var b2 = math.matrix([0, 1.6]);
-var b3 = math.matrix([0, 0.44]);
-var b4 = math.matrix([0, 0]);
+// Define the affine transformation parameters.
+var A = new Array(
+    math.matrix([
+        [0.85, 0.04],
+        [-0.04, 0.85]
+    ]),
+    math.matrix([
+        [0.2, -0.26],
+        [0.23, 0.22]
+    ]),
+    math.matrix([
+        [-0.15, 0.28],
+        [0.26, 0.24]
+    ]),
+    math.matrix([
+        [0, 0],
+        [0, 0.16]
+    ])
+);
+
+var b = new Array(
+    math.matrix([0, 1.6]), 
+    math.matrix([0, 1.6]),
+    math.matrix([0, 0.44]),
+    math.matrix([0, 0])
+);
 
 // Probabilities
 var p = [0.85, 0.92, 0.99, 1.00];
 
-// Transformation calculations.
-// FIXME: There must be a smarter way of doing this in JS!
-function trans1(vec) {
-    return(math.add(math.multiply(A1, vec), b1));
+// Transformation calculation
+function trans(i, vec) {
+    return(math.add(math.multiply(A[i], vec), b[i]));
 }
-
-function trans2(vec) {
-    return(math.add(math.multiply(A2, vec), b2));
-}
-
-function trans3(vec) {
-    return(math.add(math.multiply(A3, vec), b3));
-}
-
-function trans4(vec) {
-    return(math.add(math.multiply(A4, vec), b4));
-}
-
 
 function transform_point(vec) {
     var r = math.random();
 
     if (r < p[0]) {
-        return(trans1(vec));
+        return(trans(0, vec));
     } else if (r < p[1]) {
-        return(trans2(vec));
+        return(trans(1, vec));
     } else if (r < p[2]) {
-        return(trans3(vec));
+        return(trans(2, vec));
     } else {
-        return(trans4(vec));
+        return(trans(3, vec));
     }
 }
 
 
 function pic_col(iter, max_iter) {
-    var COL1 = color(4, 91, 0);
-    var COL2 = color(9, 183, 0);
-    var COL3 = color(12, 249, 0);
-    var COL4 = color(90, 255, 81);
+    var COLS = new Array(
+        color(4, 91, 0),
+        color(9, 183, 0),
+        color(12, 249, 0),
+        color(90, 255, 81)
+    );
     
     var p = iter / max_iter;
     
     if (p < 0.25) {
-        stroke(COL1);
+        stroke(COLS[0]);
     } else if (p < 0.5) {
-        stroke(COL2);
+        stroke(COLS[1]);
     } else if (p < 0.75) {
-        stroke(COL3);
+        stroke(COLS[2]);
     } else {
-        stroke(COL4);
+        stroke(COLS[3]);
     }
 }
 
-function draw_points() {
+function draw_points(screenx, screeny) {
     // Initial values.
-    
-    var SCALE = 50;
-    var CENTERX = 400;
-    var CENTERY = 600;
-    var MAX_ITER = 100000;
-   
-    var vec = math.matrix([0, 0]);
+    var centerx = screenx / 2;
+    var centery = screeny;
     var iter = MAX_ITER;
-  
-
+    var vec = math.matrix([0, 0]);
+    
     while(iter > 0) {
         var v = vec.valueOf();
         pic_col(iter, MAX_ITER);
-        point(CENTERX + SCALE * v[0], CENTERY - SCALE * v[1]);
+        point(centerx + SCALE * v[0], centery - SCALE * v[1]);
         vec = transform_point(vec);
         iter--;
     }
@@ -94,11 +98,13 @@ function draw_points() {
 
 // Main p5.js functions.
 function setup() {
-    createCanvas(800, 600)
+    createCanvas(SCREENX, SCREENY);
     background(0);
-    draw_points();
 }
 
 function draw() {
-
+    draw_points(SCREENX, SCREENY);
+    
+    // Stop execution.
+    noLoop();
 }
